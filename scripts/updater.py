7fc4e0db734e1395df7c86d79d456f221ee1186a
Gvinfinity <bielviniciussoares@gmail.com>
2021-10-21 15:48:57 -0300

    Changed to using python script and label check

---
+++ scripts/updater.py
@@ -0,0 +1,23 @@
+import os, json
+
+def main():
+    repo = os.getenv("repo")
+    assignees = json.loads(os.getenv("assignees"))
+    _readme = open("../README.md", "a")
+
+    contributorsList = []
+    for assignee in assignees:
+        login = assignee.get("login")
+        url = assignee.get("html_url")
+        s = f"[{login}]({url})"
+        contributorsList.append(s)
+    
+    contributorsString = ", ".join(contributorsList)
+
+    string = f"\n### [{repo}](https://github.com/ifpeopensource/workshops/tree/main/{repo}), com {contributorsString}"
+
+    _readme.write(string)
+    _readme.close()
+
+if __name__ == "__main__":
+    main()    
